a3729e3756230beea8d6ad340801df0e6bf77f1b
sahadat96 <sahadatappdev@gmail.com>
2026-02-27 16:16:48 +0600

    write a function in UserRepository

--- src/modules/auth/infrastructure/repositories/user.repository.ts
+++ src/modules/auth/infrastructure/repositories/user.repository.ts
@@ -1,2 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { IUserRepository } from "../../domain/interfaces/user.repository.interface";
+import { User } from "../../domain/entities/user.entity"
+
+@Injectable()
+exports class UserRepository implements IUserRepository {
+    constructor(private readonly prisma: PrismaService){}
+    
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+}
